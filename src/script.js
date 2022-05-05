ed654f0068ade29a895903a08e920db9177e8b04
Icaro Melchior <icaro_barbosa@outlook.com>
2022-05-05 01:21:42 -0300

    feat(add-menu-media-query):js

--- src/script.js
+++ src/script.js
@@ -1,3 +1,17 @@
+// MENU AMBURGER
+$('.navbar__menu-btn').on('click', function() {
+  $('.navbar__links').toggleClass('active');
+  $(this).find('i').toggleClass('fa-bars');
+  $(this).find('i').toggleClass('fa-times');
+});
+
+
+
+
+
+
+// CARROCEL
+
 $(document).ready(function(){
   $('.slider').slick({
     autoplay: true,
